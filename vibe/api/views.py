d2418e5c590fdb695e298e3a7f84a53f4c55dd5d
Ruppy <rupeshk7778@gmail.com>
2023-07-05 12:40:01 +0530

    Initial commit

---
+++ vibe/api/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .models import Room
+from .serializers import RoomSerializer
+
+# Create your views here.
+
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
